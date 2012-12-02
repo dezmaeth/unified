016e89f1a04611448890636063cd82503d374796
root <root@hivecluster.mcast.cl>
2012-12-02 12:31:10 -0300

    changes

--- controllers/parser.php
+++ controllers/parser.php
@@ -4,14 +4,44 @@
 Class Controller extends Controllers {
 
 	function __construct() {
-		header("Content-Type: application/json");
+		
 	}
 
 	function GET($r,$p) {
-		$this->__autoload("Db");
-		$list = $this->Db->exeQ("SELECT * FROM LISTADO_CONSULTORIOS");
+
+		if (isset($p[2])) {
+			$this->__autoload("Db");
+                	$list = (object) $this->Db->exeQ("SELECT * FROM `LISTADO_CONSULTORIOS`");
+
+			switch($p[2]) {
+
+			case "xml":
+				//header("Content-type: text/xml");
+				$xml = new SimpleXMLElement('<root/>');
+				array_walk_recursive($list, array ($xml, 'addChild'));
+				print $xml->asXML();
+
+				//print $this->array2xml($list);
+			break;
+
+			case "dump":
+				header("Content-Type: text/plain");
+				print_r($list);
+			break;
+
+			case "json":
+				header("Content-type: application/json");
+				print json_encode($list);
+			break;
+			}			
+
+
+		}
 		
-		print json_encode($list);
+	}
+
+	function POST($r,$p) {
+		
 
 	}
 }
